d1fca9b4fbdf6ade723f3572ae722e0d122f7fee
Pedro <Thanksuniverse333@outlook.com>
2022-05-14 02:46:26 -0300

    Finish -- Debug

---
+++ js/formulario.js
@@ -0,0 +1,186 @@
+/* Exercicio EBAC
+1. Verificação de número - verifyLetters
+2. Verificação de email - verifyEmail
+3. Verificação de uf  - verifyDigits
+4. Verificação de cep - verifyCep
+5. Verificação de nome - verifyLetters
+5. Verificação de cidade - verifyCity
+*/
+
+//TODO 1 : Create the basic syntex to Disallow Numbers-Symbols
+//TODO 2 : Declare the variables
+//TODO 3 : Make the form dont submit before checking all errors
+//TODO 4 : Fix the HTML/CSS part
+//TODO-5 : Change the PLACEHOLDER and add a Class to the element
+
+//*Info: verifyLetters-verifyDigits-verifyCep all almost have the same requirements
+
+//Declarations
+
+let = ValidLetters = document.querySelectorAll("input.verifyLetters");
+let = ValidEmail = document.querySelectorAll("input.verifyEmail");
+let = ValidDigits = document.querySelectorAll("input.verifyDigits");
+let = ValidCep = document.querySelectorAll("input.verifyCep");
+let = ValidCity = document.querySelectorAll("input.verifyCity");
+
+//Cancel submit when didnt meet the requirements
+
+const form0 = document.getElementById("form0");
+form0.addEventListener("submit", function (event) {
+   event.preventDefault();
+   event.stopPropagation();
+
+   if (form0.classList.contains("error"));
+   {
+      document.querySelector(".message").innerHTML = "Verifique os campos";
+      document.getElementsByName("nome")[0].placeholder = "*Nome";
+      document.querySelector('[name = "nome"]').classList.add("need");
+      document.getElementsByName("email")[0].placeholder = "*Email";
+      document.querySelector('[name = "email"]').classList.add("need");
+      document.getElementsByName("uf")[0].placeholder = "*UF";
+      document.querySelector('[name = "uf"').classList.add("need");
+      document.getElementsByName("cep")[0].placeholder = "*CEP";
+      document.querySelector('[name = "cep"]').classList.add("need");
+      document.getElementsByName("cidade")[0].placeholder = "*Cidade";
+      document.querySelector('[name= "cidade"]').classList.add("need");
+      return false;
+   }
+});
+
+//Limit Characters
+
+function LimitCharacters(doc) {
+   let = max = 2;
+
+   if (doc.value.length > max) {
+      doc.value = doc.value.substr(1, max); //Se colocar 1 apaga o primeiro número
+   }
+}
+
+//VerifyName
+
+function verifyLetters(doc) {
+   /*  1 Allow only letters
+   2 Disallow Numbers-Symbols */
+   const ValidLetters = /^[a-z]/i;
+   doc.addEventListener("focusout", function (event) {
+      event.preventDefault();
+      if (this.value.match(ValidLetters)) {
+         document.querySelector(".message").innerHTML = "";
+         this.classList.remove("error");
+         this.parentNode.classList.remove("error");
+      } else {
+         document.querySelector(".message").innerHTML = "O campo nome é obrigatório";
+         document.getElementsByName("nome")[0].placeholder = "*Nome";
+         document.querySelector('[name = "nome"]').classList.add("need");
+         this.classList.add("error");
+         this.parentNode.classList.add("error");
+         return false;
+      }
+   });
+}
+
+//VerifyEmail
+
+function verifyEmail(doc) {
+   /* 1 Basically get the RegEx from the other script and incorporate the code */
+   const ValidEmail = /[@.com]/;
+   doc.addEventListener("focusout", function (event) {
+      event.preventDefault();
+      if (this.value.match(ValidEmail) && doc.value.length > 5) {
+         document.querySelector(".message").innerHTML = "";
+         this.classList.remove("error");
+         this.parentNode.classList.remove("error");
+      } else {
+         document.querySelector(".message").innerHTML = "O campo email é obrigatório";
+         document.getElementsByName("email")[0].placeholder = "*Email";
+         document.querySelector('[name = "email"]').classList.add("need");
+         this.classList.add("error");
+         this.parentNode.classList.add("error");
+      }
+   });
+}
+
+//VerifyUF
+
+function verifyDigits(doc) {
+   /* 1 Allow only two digits inside the whole string
+   2 Disallow Numbers-Symbols */
+   const ValidDigits = /^[A-Z]/;
+   doc.addEventListener("focusout", function (event) {
+      event.preventDefault();
+      if (this.value.match(ValidDigits) && doc.value.length == 2) {
+         document.querySelector(".message").innerHTML = "";
+         this.classList.remove("error");
+         this.parentNode.classList.remove("error");
+      } else {
+         document.querySelector(".message").innerHTML = "O campo UF é obrigatório";
+         document.getElementsByName("uf")[0].placeholder = "*UF";
+         document.querySelector('[name = "uf"]').classList.add("need");
+         this.classList.add("error");
+         this.parentNode.classList.add("error");
+      }
+   });
+}
+
+//VerifyCep
+
+function verifyCep(doc) {
+   /* 1 Get the base RegEx from the other script
+   2 Block any letter-symbols */
+   const ValidCep = /^\d{5}(?:[-\s]\d{4})?$/;
+   doc.addEventListener("focusout", function (event) {
+      event.preventDefault();
+      if (this.value.match(ValidCep)) {
+         document.querySelector(".message").innerHTML = "";
+         this.classList.remove("error");
+         this.parentNode.classList.remove("error");
+      } else {
+         document.querySelector(".message").innerHTML = "O campo cep é obrigatório";
+         document.getElementsByName("cep")[0].placeholder = "*CEP";
+         document.querySelector('[name = "cep"]').classList.add("need");
+         this.classList.add("error");
+         this.parentNode.classList.add("error");
+      }
+   });
+}
+
+//VerifyCity
+
+function verifyCity(doc) {
+   /* 1 Verify Letters
+      2 Verify Min Length of 3*/
+   const ValidCity = /[a-z]/i;
+   doc.addEventListener("focusout", function (event) {
+      event.preventDefault();
+      if (this.value.match(ValidCity)) {
+         document.querySelector(".message").innerHTML = "";
+         this.classList.remove("error");
+         this.parentNode.classList.remove("error");
+      } else {
+         document.querySelector(".message").innerHTML = "O campo cidade é obrigatório";
+         document.getElementsByName("cidade")[0].placeholder = "*Cidade";
+         document.querySelector('[name = "cidade"]').classList.add("need");
+         this.classList.add("error");
+         this.parentNode.classList.add("error");
+      }
+   });
+}
+
+//Loops
+
+for (let Focused of ValidLetters) {
+   verifyLetters(Focused);
+}
+for (let Focused of ValidEmail) {
+   verifyEmail(Focused);
+}
+for (let Focused of ValidDigits) {
+   verifyDigits(Focused);
+}
+for (let Focused of ValidCep) {
+   verifyCep(Focused);
+}
+for (let Focused of ValidCity) {
+   verifyCity(Focused);
+}
--- js/script.js
+++ js/script.js
@@ -1,61 +1,66 @@
 /*
 Formulário envio de dados para cálculo da Média
 */
+const form01 = document.getElementById("form01");
+if (form01)
+   form01.addEventListener("submit", function (evento) {
+      evento.preventDefault(); //Formulário para de ter o comportamento padrão do html
+      evento.stopPropagation(); //Faz com que todo submit de form caia nessa função
 
-document.getElementById("form-01").addEventListener("submit", function (evento) {
-   evento.preventDefault(); //Formulário para de ter o comportamento padrão do html
-   evento.stopPropagation(); //Faz com que todo submit de form caia nessa função
+      if (this.classList.contains("erro")) {
+         return false;
+      }
 
-   if (this.getAttribute("class").match(/erro/)) {
+      let dados = new FormData(this);
+
+      let notas = [];
+
+      for (let key of dados.keys()) {
+         let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // ParseFloat - Número com ponto depois do decimal
+         // .match(/\d/) verifica se contem um digito se sim transforma em um número se não transforma em 0
+         console.log(typeof numero);
+
+         if (!isNaN(numero)) {
+            notas.push(numero);
+         }
+      }
+
+      console.log(notas);
+
+      function calcularMedia(notas) {
+         var soma = 0;
+         for (c = 0; c < notas.length; c++) {
+            soma += notas[c];
+         }
+
+         media = soma / notas.length; //Utilizar notas.length para que ele calcule dinamicamente a quantidade que ela recebeu
+
+         return media;
+      }
+
+      function aprovacao(notas) {
+         let media = calcularMedia(notas);
+
+         if (media >= 7) {
+            condicao = "Aprovado";
+            document.getElementById("resultado").style.backgroundColor = "Green";
+         } else if (media >= 4) {
+            condicao = "Recuperação";
+            document.getElementById("resultado").style.backgroundColor = "Yellow";
+         } else {
+            condicao = "Reprovado";
+            document.getElementById("resultado").style.backgroundColor = "Red";
+         }
+         return "Média: " + media + " - Resultado: " + condicao;
+      }
+
+      document.getElementById("resultado").innerHTML = aprovacao(notas);
+      aprovacao(notas);
+   });
+
+/* if (this.getAttribute("class").match(/erro/)) {
       return false;
-   }
-
-   let dados = new FormData(this);
-
-   let notas = [];
-
-   for (let key of dados.keys()) {
-      let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // ParseFloat - Número com ponto depois do decimal
-      // .match(/\d/) verifica se contem um digito se sim transforma em um número se não transforma em 0
-      console.log(typeof numero);
-
-      if (!isNaN(numero)) {
-         notas.push(numero);
-      }
-   }
-
-   console.log(notas);
-
-   function calcularMedia(notas) {
-      var soma = 0;
-      for (c = 0; c < notas.length; c++) {
-         soma += notas[c];
-      }
-
-      media = soma / notas.length; //Utilizar notas.length para que ele calcule dinamicamente a quantidade que ela recebeu
-
-      return media;
-   }
-
-   function aprovacao(notas) {
-      let media = calcularMedia(notas);
-
-      if (media >= 7) {
-         condicao = "Aprovado";
-         document.getElementById("resultado").style.backgroundColor = "Green";
-      } else if (media >= 4) {
-         condicao = "Recuperação";
-         document.getElementById("resultado").style.backgroundColor = "Yellow";
-      } else {
-         condicao = "Reprovado";
-         document.getElementById("resultado").style.backgroundColor = "Red";
-      }
-      return "Média: " + media + " - Resultado: " + condicao;
-   }
-
-   document.getElementById("resultado").innerHTML = aprovacao(notas);
-   aprovacao(notas);
-});
+   } */
 
 function Validation(elemento) {
    elemento.addEventListener("focusout", function (event) {
@@ -75,9 +80,10 @@ function Validation(elemento) {
 }
 
 function NumericValidation(elemento) {
+   let numero = this.value.match(/^[\d]5-[\d]/) ? this.value.replace(/-/, "") : this.value; //!Colocar na verificação de CEP para que possa colocar um traço //
    elemento.addEventListener("focusout", function (event) {
       event.preventDefault();
-      if (this.value != "" && this.value.match(/[0-9]*/) && this.value >= 0 && this.value <= 10) {
+      if (numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10) {
          document.querySelector(".mensagem").innerHTML = "";
          this.classList.remove("erro");
          this.parentNode.classList.remove("erro");
@@ -90,8 +96,27 @@ function NumericValidation(elemento) {
    });
 }
 
+function EmailValidation(elemento) {
+   const emailValido = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i; //Flag i significa para ignorar se for fonte maiuscula ou minuscula
+   elemento.addEventListener("focusout", function (event) {
+      event.preventDefault();
+
+      if (this.value.match(emailValido)) {
+         document.querySelector(".mensagem").innerHTML = "O campo é apenas numérico";
+         this.classList.add("erro");
+         this.parentNode.classList.add("erro"); //Adiciona Classe no ParerntNode(Pai) no caso no formulário, impedindo que envie o formulário
+         return false;
+      } else {
+         document.querySelector(".mensagem").innerHTML = "";
+         this.classList.remove("erro");
+         this.parentNode.classList.remove("erro");
+      }
+   });
+}
+
 let Necessary = document.querySelectorAll("input.obrigatorio");
 let Numeric = document.querySelectorAll("input.numero");
+let Email = document.querySelectorAll("input.email");
 
 for (let Focused of Necessary) {
    Validation(Focused);
@@ -100,3 +125,9 @@ for (let Focused of Necessary) {
 for (let Focused of Numeric) {
    NumericValidation(Focused);
 }
+
+for (let Focused of Email) {
+   EmailValidation(Focused);
+}
+
+//
